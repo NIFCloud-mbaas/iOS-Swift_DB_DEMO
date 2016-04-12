b250ecaacc7de440e5e807c4f620e4dd45dbd950
natsumo <ikedana@systena.co.jp>
2016-04-12 17:24:20 +0900

    2 commit
    20160412-2

---
+++ DB_Swift/DB_Swift/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  DB_Swift
+//
+//  Created by Natsumo Ikeda on 2016/04/12.
+//  Copyright © 2016年 NIFTY Corporation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func startBtn(sender: UIButton) {
+        // 保存先クラスを作成
+        let obj = NCMBObject(className: "TestClass")
+        // 値を設定
+        obj.setObject("Hello,NCMB!", forKey: "message")
+        // 保存を実施
+        obj.saveInBackgroundWithBlock{(error: NSError!) in
+            if (error != nil) {
+                // 保存に失敗した場合の処理
+                print("エラーが発生しました。エラーコード:\(error.code)")
+                self.alertText.text = "エラーが発生しました。エラーコード:\(error.code)"
+            }else{
+                // 保存に成功した場合の処理
+                print("保存に成功しました。objectId:\(obj.objectId)")
+                self.alertText.text = "保存に成功しました。objectId:\(obj.objectId)"
+            }
+        }
+
+        
+    }
+    @IBOutlet weak var alertText: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
--- README.md
+++ README.md
@@ -1,8 +1,36 @@
-# 【Swift体験会】ニフティクラウドmobile backend を使ってみよう！
+# 【iOS Swift】ニフティクラウドmobile backend を体験しよう！
+![画像1](/readme-img/001.png)
 
 ## 概要
-## 内容
+* Xcode(Swift)で作成したiOSアプリから、[ニフティクラウドmobile backend](http://mb.cloud.nifty.com/)へデータ登録を行うサンプアプリです
+ * 「START DEMO」ボタンをタップするとクラウドにデータが上がります★
+* 簡単な操作ですぐに [ニフティクラウドmobile backend](http://mb.cloud.nifty.com/)を体験いただけます
+
+## ニフティクラウドmobile backendって何？？
+スマートフォンアプリのバックエンド機能（プッシュ通知・データストア・会員管理・ファイルストア・SNS連携・位置情報検索・スクリプト）が**開発不要**、しかも**無料**_(※1)_で使えるクラウドサービス！今回はデータストアを体験します
+
+![画像2](/readme-img/002.png)
+
+## 動作環境
+* Mac OS X 10.10(Yosemite)
+* Xcode ver.7.2.1
+
+※上記内容で動作確認をしています。
+
+
 ## 手順
+1. [ニフティクラウドmobile backend](http://mb.cloud.nifty.com/)の会員登録とログイン→アプリ作成
+1. [GitHub](https://github.com/natsumo/iOS-Swift_DB_DEMO.git)からサンプルプロジェクトのダウンロード
+1. Xcodeでアプリを起動
+1. APIキーの設定
+1. 動作確認
+
+
 ## 解説
+プロジェクトに実装済みの内容のご紹介
+* SDKのインポート
+* ロジック
+
+
 ## 参考
 
